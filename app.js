e1f7e29501a79ab06b4fcc458a1b11bfa4f174cf
YankoChergov55 <chergov.yanko.18@gmail.com>
2024-11-08 12:50:02 +0200

    finished the project

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log('Welcome to my portfolio!');
+
+const darkmodeButton = document.getElementById('mode');
+console.log(darkmodeButton);
+
+darkmodeButton.addEventListener('click', () => {
+	document.body.classList.toggle('darkmode');
+});
